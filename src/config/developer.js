8e582738deb2e1463594f3e7970de52b17b14a34
foregato <arthurforegato@hotmail.com>
2026-08-03 11:14:13 -0300

    espaço minimo

--- src/config/developer.js
+++ src/config/developer.js
@@ -1,4 +1,4 @@
 // Crédito do desenvolvedor — deve permanecer em todas as páginas.
 // Alterar a URL abaixo é suficiente para atualizar o link em todo o site.
-export const DEVELOPER_NAME = "Forecatt";
+export const DEVELOPER_NAME = " Forecatt";
 export const DEVELOPER_URL = "";
